934dc109afe193d0a5267e9f91a527c0ea7769c7
Andres Perez Hortal <16256571+aperezhortal@users.noreply.github.com>
2022-01-03 12:09:10 -0500

    Install pysteps data

---
+++ ci/fetch_pysteps_data.py
@@ -0,0 +1,21 @@
+"""
+Install the pysteps data in a test environment and create a pystepsrc
+configuration file pointing to that data.
+"""
+
+if __name__ == "__main__":
+    import argparse
+    from pysteps.datasets import create_default_pystepsrc, download_pysteps_data
+
+    parser = argparse.ArgumentParser(description="Install pysteps data")
+
+    parser.add_argument(
+        "dest_dir", type=str, help="Directory where to install the Pysteps data"
+    )
+    args = parser.parse_args()
+
+    download_pysteps_data(args.dest_dir, force=True)
+
+    create_default_pystepsrc(
+        args.dest_dir, config_dir=args.dest_dir, file_name="pystepsrc"
+    )
