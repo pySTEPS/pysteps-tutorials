0a2a8417c2c6a6b9db311760abe6710bc9bfafb6
Andres Perez Hortal <16256571+aperezhortal@users.noreply.github.com>
2022-01-03 19:12:21 -0500

    Use sphinx-gallery and scripts instead of notebooks

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = "pysteps_tutorials"
+copyright = "2022, Pysteps developers"
+author = "Pysteps developers"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx_gallery.gen_gallery",
+]
+
+sphinx_gallery_conf = {
+    "examples_dirs": "../examples",
+    "gallery_dirs": "examples_gallery",
+    "filename_pattern": r"/*\.py",
+}
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
